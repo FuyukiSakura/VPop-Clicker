76fc36eae1483f2020a8524c2522e77c6b2ab36d
Fuyuki Sakura <fuyuki@sakura.live>
2024-05-26 16:56:16 +1000

    fix tailwindcss not compiling properly

--- VPop.Clicker/tailwind.config.js
+++ VPop.Clicker/tailwind.config.js
@@ -1,5 +1,6 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    purge: ['./**/*.html'],
+    content: ["./**/*.{razor,html,cshtml}"],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
